8d7dbd266206a3c0276f9b2759ea382383bfdfa0
NguyenAnhDuc <ducna01120@gmail.com>
2015-02-02 19:41:38 +0700

    update

---
+++ www/js/chat.js
@@ -0,0 +1,59 @@
+/**
+ * Created by DucNa on 1/12/15.
+ */
+function buildHtmlQuestion(question) {
+    var htmlResult = "";
+    //question
+    htmlResult = htmlResult
+        .concat("<ons-list-item class=\"ons-list-item right clearfix list__item ons-list-item-inner left clearfix\"><span class=\"chat-img pull-left\">");
+    htmlResult = htmlResult
+        .concat("<img src=\"img/u.gif\" alt=\"User Avatar\" class=\"img-circle\" /></span>");
+    htmlResult = htmlResult
+        .concat("<div class=\"chat-body clearfix\" style=\"margin-left: 60px;\" >");
+    htmlResult = htmlResult.concat("<p>");
+    htmlResult = htmlResult.concat(question);
+    htmlResult = htmlResult.concat("</p></div></ons-list-item >");
+    return htmlResult;
+}
+function buildHtmlAnswer( text) {
+    var htmlResult = "";
+    //answer
+    htmlResult = htmlResult
+        .concat("<ons-list-item  class=\"ons-list-item right clearfix list__item ons-list-item-inner right clearfix\"><span class=\"chat-img pull-right\">");
+    htmlResult = htmlResult
+        .concat("<img src=\"img/me.gif\" alt=\"User Avatar\" class=\"img-circle\" /></span>");
+    htmlResult = htmlResult
+        .concat("<div class=\" chat-body clearfix\">");
+    htmlResult = htmlResult.concat("<p id=\"chat-answer\">");
+    htmlResult = htmlResult.concat("<div class=\"spinner\" id=\"loading\">	<div class=\"bounce1\"></div> <div class=\"bounce2\"></div>	<div class=\"bounce3\"></div></div>");
+    htmlResult = htmlResult.concat("</p></div></ons-list-item >");
+    return htmlResult;
+}
+
+function onChat() {
+    $('#chat-answer').attr('id','old-chat-answer');
+    $('#loading').attr('id','old-loading')
+    var question = $('#btn-input').val();
+    $('#btn-input').val('');
+    $('#currentqs').val(question);
+    $('.panel-body').scrollTop(1E10);
+    var oldHeight = $('.panel-body').scrollTop();
+    var htmlResult = buildHtmlQuestion(question).concat(buildHtmlAnswer("ME"));
+    $('.chat').append(htmlResult);
+    $('.panel-body').scrollTop(oldHeight+400);
+    $('#loading').show();
+    $
+        .ajax({
+            type: "POST",
+            url: HOST + "/getAnswer",
+            contentType: "application/x-www-form-urlencoded;charset=UTF-8",
+            data: "question=" + encodeURIComponent(question) + "&confirmWebSearch=" + encodeURIComponent("yes"),
+            success: function (result) {
+                $('.spinner').hide();
+                $('#chat-answer').html(result.answer);
+            },
+            error: function (result) {
+                alert("Error");
+            }
+        });
+}
---
+++ www/js/main.js
@@ -0,0 +1,124 @@
+(function () {
+    'use strict';
+    var module = angular.module('app', ['onsen']);
+    var networkErrorString = "Tôi không thể tìm thấy kết nối internet, bạn hãy kiểm tra lại được không?";
+    var preFrqQuestions = {};
+    var randomQuestions = {};
+    var rubyDialog = {};
+    $(document).ready(function () {
+        // call ajax to get list frequently question
+
+        $.ajax({
+            type: "GET",
+            dataType: "text",
+            url: HOST + "/info/frequent",
+            success: function (result) {
+                preFrqQuestions.items = JSON.parse(result);
+            },
+            error: function(){
+               ons.createAlertDialog('alert.html').then(function(alertDialog) {
+                   rubyDialog.dialog = alertDialog;
+                   alertDialog.show();
+                });
+            },
+            timeout : TIME_OUT
+        });
+
+        $.ajax({
+            type: "GET",
+            dataType: "text",
+            url: HOST + "/info/random",
+            success: function (result) {
+                randomQuestions.items = JSON.parse(result);
+            },
+            timeout : TIME_OUT
+        });
+    });
+
+    module.controller('AlertController', function ($scope) {
+        $scope.closeAlert = function() {
+            if(rubyDialog.dialog.alertDialog && rubyDialog.dialog.alertDialog.isShown()) {
+                rubyDialog.dialog.alertDialog.hide();
+            }
+        };
+    });
+
+    module.controller('FrqQuestionController', function ($scope, $frqQuestions) {
+        $scope.items = $frqQuestions.items;
+
+        $scope.btnQuestionClick = function () {
+            ons.navigator.popPage();
+            mixpanel.track("ask", {'input': 'freq'});
+            request(this.item.question);
+        }
+
+        $scope.showMainPage = function () {
+            ons.navigator.popPage();
+        }
+
+    });
+
+
+
+    module.controller('MainCtrl', function ($scope) {
+        ons.createDialog('dialog.html').then(function (dialog) {
+            $scope.dialog = dialog;
+        });
+
+        ons.createDialog('seemore.html').then(function (dialog) {
+            $scope.dialog_seemore = dialog;
+        });
+        $scope.showRandom = function () {
+            $('.dialog-mask').show();
+            $scope.dialog.show();
+            $('.random-question').show();
+            var items = randomQuestions.items;
+            $('#random-question-1').html(items[getRandomInt(0,9)]);
+            $('#random-question-2').html(items[getRandomInt(10,19)]);
+            $('#random-question-3').html(items[getRandomInt(20,29)]);
+            for (var i=1;i<=3;i++){
+                $('#random-question-' + i).css('line-height','36px');
+                if ($('#random-question-'+i).height() > 36) $('#random-question-'+i).css('line-height','18px');
+                if ($('#random-question-'+i).height() === 18) $('#random-question-'+i).css('line-height','36px');
+            }
+            mixpanel.track("viewRandom");
+
+        }
+        $scope.seemore = function (answer) {
+            mixpanel.track("seemore", {});
+            $scope.dialog_seemore.show();
+
+
+        }
+
+
+        $scope.showInput = function () {
+            $('#footer').height('20%');
+            $('#input').hide();
+            $('.show-question').show();
+            $('.button-request').show();
+            $('#button-request').show();
+        }
+
+        $scope.showFrqPage = function () {
+            mixpanel.track("viewFrequent");
+            ruby.navigator.pushPage('html/frq-questions.html', {title: 'frequently asked questions'})
+        }
+
+
+        $scope.closeAlert = function() {
+            if(rubyDialog.dialog.alertDialog && rubyDialog.dialog.alertDialog.isShown()) {
+                rubyDialog.dialog.alertDialog.hide();
+            }
+        };
+    });
+
+
+    module.factory('$frqQuestions', function () {
+        var data = {};
+        data.items = preFrqQuestions.items;
+        return data;
+    });
+
+
+})();
